62d5848621a268503e6d396e6560cb8e730ca2ef
Kenneth <krbrown@live.ca>
2018-01-18 21:49:21 +0000

    map function complete

---
+++ mapFunction.js
@@ -0,0 +1,24 @@
+
+
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+function map(array, callback) {
+	var newArray = [];
+	array.forEach(function(element){
+		newArray.push(callback(element));		
+	});
+	return newArray;
+}
+
